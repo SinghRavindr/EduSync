983207d75f32162ae890d4f9ef6f851e56c6c4a4
varuns2107 <singhal1varun725@gmail.com>
2025-12-22 01:24:00 +0530

    developers.jsx

--- src/components/developers.jsx
+++ src/components/developers.jsx
@@ -30,7 +30,7 @@ export default function Developers() {
         {/* Render individual developer components */}
         <div className="items-center justify-center max-w-[30rem] max-h-[30rem] mb-10 mt-5">
           <BackgroundGradientDemo
-            imgSrc="/devs/varun.jpg"
+            imgSrc="/devs/varun.jpeg"
             description="Varun Singhal"
             githubLink="https://github.com/varuns2107"
             linkedinLink="https://www.linkedin.com/in/varun-singhal-747a44284/"
@@ -39,7 +39,7 @@ export default function Developers() {
         </div>
         <div className="items-center justify-center max-w-[30rem] max-h-[30rem] mb-10 mt-5">
           <BackgroundGradientDemo
-            imgSrc="/devs/ravindra.png"
+            imgSrc="/devs/ravindra.jpg"
             description="Ravindra Singh"
             githubLink="https://github.com/SinghRavindr"
             linkedinLink="https://www.linkedin.com/in/ravindra-singh-31041332b/"
